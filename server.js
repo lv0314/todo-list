c58105503bc40ae0f53d51f9775a87bd2b7de8da
DESKTOP-88VMMCI\user <luzverde0314@gmail.com>
2022-04-11 22:41:25 +0900

    .

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URI || mongoUri, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("Connected to Database"));
